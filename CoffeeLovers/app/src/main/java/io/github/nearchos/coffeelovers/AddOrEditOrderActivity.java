855c487910c7a67f4cbf26cb201fd04343a301f4
Nearchos Paspallis <nearchos@aspectsense.com>
2018-03-29 12:04:08 +0300

    Adding CoffeeLovers app

---
+++ CoffeeLovers/app/src/main/java/io/github/nearchos/coffeelovers/AddOrEditOrderActivity.java
@@ -0,0 +1,13 @@
+package io.github.nearchos.coffeelovers;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AddOrEditOrderActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_or_edit_order);
+    }
+}
---
+++ CoffeeLovers/app/src/main/java/io/github/nearchos/coffeelovers/model/Coffee.java
@@ -0,0 +1,45 @@
+package io.github.nearchos.coffeelovers.model;
+
+/**
+ * @author Nearchos
+ * Created: 29-Mar-18
+ */
+public class Coffee {
+
+    public enum Type { CAPPUCCINO, LATTE, AMERICANO };
+    public enum Size { SMALL, MEDIUM, LARGE };
+    public enum Milk { NO_MILK, WHOLE_MILK, SEMI_SKIMMED_MILK, SKIMMED_MILK, LACTOSE_FREE_MILK, SOYA_BASED_MILK };
+
+    private Type type;
+    private Size size;
+    private Milk milk;
+    private boolean caffeineFree;
+
+    public Coffee(Type type, Size size, Milk milk, boolean caffeineFree) {
+        this.type = type;
+        this.size = size;
+        this.milk = milk;
+        this.caffeineFree = caffeineFree;
+    }
+
+    public Type getType() {
+        return type;
+    }
+
+    public Size getSize() {
+        return size;
+    }
+
+    public Milk getMilk() {
+        return milk;
+    }
+
+    public boolean isCaffeineFree() {
+        return caffeineFree;
+    }
+
+    @Override
+    public String toString() {
+        return size + " " + type + (caffeineFree ? " caffeine free, " : ", ") + " with " + milk;
+    }
+}
