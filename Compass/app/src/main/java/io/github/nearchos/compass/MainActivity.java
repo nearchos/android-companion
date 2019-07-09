4ef75b8552a4fbe0180bb89d3030e581223017bf
Nearchos Paspallis <npaspallis@uclan.ac.uk>
2019-07-09 17:14:26 +0300

    Updating the Compass project

--- Compass/app/src/main/java/io/github/nearchos/compass/MainActivity.java
+++ Compass/app/src/main/java/io/github/nearchos/compass/MainActivity.java
@@ -1,13 +1,53 @@
 package io.github.nearchos.compass;
 
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.Locale;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+
+    private TextView headingTextView;
+    private ImageView compassImageView;
+    private SensorManager sensorManager;
+
+    @Override protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.headingTextView = findViewById(R.id.headingTextView);
+        this.compassImageView = findViewById(R.id.compassImageView);
+
+        this.sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+    }
+
+    @Override protected void onStart() {
+        super.onStart();
+        // register for updates from the 'orientation' sensor
+        sensorManager.registerListener(this,
+                sensorManager.getDefaultSensor(Sensor.TYPE_ORIENTATION),
+                SensorManager.SENSOR_DELAY_GAME);
+    }
+
+    @Override protected void onStop() {
+        sensorManager.unregisterListener(this);
+        super.onStop();
+    }
+
+    @Override public void onSensorChanged(SensorEvent sensorEvent) {
+        final float headingInDegrees = sensorEvent.values[0]; // compass in radians
+        update(headingInDegrees);
+    }
+
+    @Override public void onAccuracyChanged(Sensor sensor, int i) { /* ignore */ }
+
+    private void update(final float headingInDegrees) {
+        headingTextView.setText(String.format(Locale.US, "Your phone is heading towards %.1f°", headingInDegrees));
     }
 }
