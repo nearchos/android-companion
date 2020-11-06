560dce2cc4b397f0cc541f4ceaadeeb907d9954a
Nearchos Paspallis <npaspallis@uclan.ac.uk>
2020-11-06 10:46:43 +0200

    Updates to Hello Android (adding androidX)

--- HelloAndroid/app/src/main/java/io/github/nearchos/helloandroid/MainActivity.java
+++ HelloAndroid/app/src/main/java/io/github/nearchos/helloandroid/MainActivity.java
@@ -1,8 +1,8 @@
 package io.github.nearchos.helloandroid;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
