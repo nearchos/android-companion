5df32c38023bee8323393c47ec2e1d8961a9d5e4
Nearchos Paspallis <npaspallis@uclan.ac.uk>
2020-11-15 11:28:15 +0200

    Migrating UIDesign project to AndroidX

--- UIDesign/app/src/main/java/io/github/nearchos/uidesign/RelativeActivity.java
+++ UIDesign/app/src/main/java/io/github/nearchos/uidesign/RelativeActivity.java
@@ -1,10 +1,11 @@
 package io.github.nearchos.uidesign;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class RelativeActivity extends AppCompatActivity {
 
