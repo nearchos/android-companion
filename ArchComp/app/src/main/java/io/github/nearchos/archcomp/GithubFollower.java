a6416f4701de9905181eb18b8165f5b9e119ae94
Nearchos Paspallis <npaspallis@uclan.ac.uk>
2021-01-09 16:28:59 +0200

    adding ArchComp project

--- ArchComp/app/src/main/java/io/github/nearchos/archcomp/GithubFollower.java
+++ ArchComp/app/src/main/java/io/github/nearchos/archcomp/GithubFollower.java
@@ -1,3 +1,12 @@
+/*
+ * Copyright (c) 2020. Nearchos Paspallis
+ * This work is licensed under the Creative Commons Attribution-NonCommercial-NoDerivs 3.0 United
+ * States License, described at this URL: https://creativecommons.org/licenses/by-nc-nd/3.0/us/.
+ * Unless required by applicable law or agreed to in writing, code listed in this site is
+ * distributed on "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied.
+ */
+
 package io.github.nearchos.archcomp;
 
 import androidx.room.Entity;
